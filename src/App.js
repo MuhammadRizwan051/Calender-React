c37460519cf8457ac9aa53ecd19d93dab8897838
MuhammadRizwan051 <memonrizwan051@gmail.com>
2023-01-17 12:55:10 +0500

    Changings

--- src/App.js
+++ src/App.js
@@ -9,19 +9,31 @@ function App() {
 
   let [monthLength, setMonthLength] = useState(0)
   let [totalDay, setTotalDay] = useState('')
+  let [fullYear, setFullYear] = useState(2023)
+  let [monthStart, setMonthStart] = useState()
 
   const getDays = () => {
-    let day = new Date(2023, monthLength + 1, 0)
+    let day = new Date(fullYear, monthLength + 1, 0)
+    console.log(day)
     setTotalDay(day.getDate())
+    setMonthStart(day.getDay())
+    setFullYear(day.getFullYear())
+
+    let currentDate = new Date().getDate()
+    let filterCurrentDate = all.filter((e, i) => {
+      return e == currentDate
+    })
+    console.log(filterCurrentDate)
+    // console.log(currentDate)
   }
 
   useEffect(() => {
     getDays()
-  }, [monthLength])
+  }, [monthLength, fullYear])
 
 
   let monthForward = () => {
-    if (monthLength == 11) {
+    if (monthLength === 11) {
       setMonthLength(0)
     }
     else {
@@ -30,12 +42,21 @@ function App() {
   }
 
   let monthBackward = () => {
-    if (monthLength == 0) {
+    if (monthLength === 0) {
       setMonthLength(11)
     }
     else {
       setMonthLength(monthLength - 1)
     }
+  }
+
+
+  let yearForward = () => {
+    setFullYear(fullYear + 1)
+  }
+
+  let yearBackward = () => {
+    setFullYear(fullYear - 1)
   }
 
   let all;
@@ -44,7 +65,7 @@ function App() {
       all = []
       for (let i = 1; i <= totalDay; i++) {
         all.push(i)
-        console.log(all)
+        // console.log(all)
       }
     })()
   }
@@ -55,26 +76,33 @@ function App() {
       <Box sx={{ height: '100vh', border: '1px solid', margin: 5 }}>
 
         <Box sx={{ width: '100%', height: '40px', display: 'flex', justifyContent: 'space-around', alignItems: 'center', paddingY: 3 }}>
-          <ion-icon name="arrow-back-outline" onClick={monthBackward}></ion-icon>
-          <Typography sx={{ fontSize: 35, fontWeight: 'bold', }}>{MONTH[monthLength]}</Typography>
-          <ion-icon name="arrow-forward-outline" onClick={monthForward}></ion-icon>
+          <Box>
+            <ion-icon name="arrow-back-outline" onClick={yearBackward}></ion-icon>
+            <ion-icon name="arrow-back-outline" onClick={monthBackward}></ion-icon>
+          </Box>
+          <Typography sx={{ fontSize: 35 }}>{MONTH[monthLength] + " " + fullYear}</Typography>
+          <Box>
+            <ion-icon name="arrow-forward-outline" onClick={monthForward}></ion-icon>
+            <ion-icon name="arrow-forward-outline" onClick={yearForward}></ion-icon>
+          </Box>
         </Box>
 
         <Grid container>
           {DAYS.map((e, i) => (
-            <Grid item xl={1.71} key={i}>
-              <Typography sx={{ textAlign: 'center', border: '1px solid black', paddingY: 3, fontSize: 30, bgcolor: 'tan' }}>{e}</Typography>
+            <Grid item sm={1.71} key={i}>
+              <Typography sx={{ textAlign: 'center', border: '1px solid black', paddingY: 3, fontSize: 30, fontWeight: 'bold' }}>{e}</Typography>
             </Grid>
           ))}
         </Grid>
 
         <Grid container sx={{ width: '100%' }}>
           {all.map((e, i) => (
-            <Grid item xl={1.71}>
+            <Grid item key={i} sm={1.71}>
               <Typography sx={{ textAlign: 'center', border: '1px solid black', paddingY: 5, fontSize: 30 }}>{e}</Typography>
             </Grid>
           ))}
         </Grid>
+
       </Box>
     </>
   )
